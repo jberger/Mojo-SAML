f250249c706ff6198a4ae0156e2bc9ecd86172a7
Joel Berger <joel.a.berger@gmail.com>
2018-04-26 17:12:53 -0500

    initial commit

---
+++ ex/jberger.pl
@@ -0,0 +1,11 @@
+use Mojo::Base -strict;
+
+use Mojo::SAML::IdP;
+
+my $idp = Mojo::SAML::IdP->new->from_url('https://keycloak.jberger.pl/auth/realms/master/protocol/saml/descriptor');
+
+say $idp->location_for('SingleSignOnService', 'HTTP-POST');
+say $idp->key_for('signing');
+say $idp->name_id_format('transient');
+say $idp->default_id_format;
+
---
+++ lib/Mojo/SAML/IdP.pm
@@ -0,0 +1,89 @@
+package Mojo::SAML::IdP;
+
+use Mojo::Base -base;
+
+use Mojo::DOM;
+use Mojo::UserAgent;
+use Scalar::Util ();
+
+my $isa = sub {
+  my ($obj, $class) = @_;
+  Scalar::Util::blessed($obj) && $obj->isa($class);
+};
+my $md = 'urn:oasis:names:tc:SAML:2.0:metadata';
+my $ds = 'http://www.w3.org/2000/09/xmldsig#';
+
+has entity_id => sub {
+  my $dom = shift->metadata;
+  my $desc = $dom->find('EntityDescriptor[entityID]')->grep(sub{ $_->namespace eq $md });
+  die 'Multiple EntityDescriptor elements found' if $desc->size > 1;
+  die 'No EntityDescriptor elements found' if $desc->size < 1;
+  return $desc->[0]->{entityID};
+};
+has metadata => sub { die 'metadata is required' };
+has ua => sub { Mojo::UserAgent->new };
+
+sub entity {
+  my $self = shift;
+  my $id = Mojo::Util::xml_escape $self->entity_id;
+  return $self->metadata->at(qq<EntityDescriptor[entityID="$id"]>) // die 'EntityDescriptor not found';
+}
+
+sub from_url {
+  my ($self, $url) = @_;
+  my $dom = $self->ua->get($url)->result->dom;
+  return $self->metadata($dom);
+}
+
+sub from_xml {
+  my ($self, $dom) = @_;
+  $dom = Mojo::DOM->new("$dom")
+    unless $dom->$isa('Mojo::DOM');
+  return $self->metadata($dom);
+}
+
+sub location_for {
+  my ($self, $service, $binding) = @_;
+  $binding = "urn:oasis:names:tc:SAML:2.0:bindings:$binding" unless $binding =~ /^\Qurn:oasis:names:tc:SAML:2.0:bindings:/;
+  my $elem = $self->entity->at(qq{IDPSSODescriptor > ${service}[Binding="$binding"][Location]});
+  return undef unless $elem && $elem->namespace eq $md;
+  return $elem->{Location};
+}
+
+sub key_for {
+  my ($self, $use) = @_;
+  $use = Mojo::Util::xml_escape $use;
+  my $elem = $self->entity->at(qq<IDPSSODescriptor KeyDescriptor[use="$use"]>);
+  return undef unless $elem && $elem->namespace eq $md;
+  my $key_elem = $elem->at(q{KeyInfo > X509Data > X509Certificate});
+  return undef unless $key_elem && $key_elem->namespace eq $ds;
+  my $key = Mojo::Util::trim $key_elem->text;
+  $key = Mojo::Util::b64_encode(Mojo::Util::b64_decode($key), ''); # clean up key
+  return "-----BEGIN CERTIFICATE-----\n$key\n-----END CERTIFICATE-----\n";
+}
+
+sub _formats {
+  my $self = shift;
+  return $self->entity
+    ->find(q<IDPSSODescriptor NameIDFormat>)
+    ->grep(sub{ $_->namespace eq $md })
+    ->map(sub{ Mojo::Util::trim $_->text });
+}
+
+sub name_id_format {
+  my ($self, $format) = @_;
+  my $formats = $self->_formats;
+  return $formats->to_array unless defined $format;
+  return $formats->first(sub{ $_ eq $format }) if $format =~ /:/;
+  $format = qr/urn:oasis:names:tc:SAML:(?:2.0|1.1):nameid-format:\Q$format/;
+  return $formats->first($format);
+}
+
+sub default_id_format {
+  my $self = shift;
+  my $formats = $self->_formats;
+  return $formats->[0];
+}
+
+1;
+
