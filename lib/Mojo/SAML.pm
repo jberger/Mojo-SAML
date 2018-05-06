4f644b3c24cec042bce8c1d913245e385f352c04
Joel Berger <joel.a.berger@gmail.com>
2018-05-06 13:55:51 -0500

    make the Mojo::SAML setup code more sane (mst++)

--- lib/Mojo/SAML.pm
+++ lib/Mojo/SAML.pm
@@ -1,28 +1,27 @@
 package Mojo::SAML;
 
-our @doc_types;
-BEGIN {
-  @doc_types = (qw/
-    KeyInfo KeyDescriptor Organization ContactPerson
-    AssertionConsumerService SPSSODescriptor EntityDescriptor
-    AuthnRequest NameIDPolicy
-  /);
-
-  for my $type (@doc_types) {
-    my $package = "Mojo::SAML::Document::$type";
-    eval "require $package";
-    die $@ if $@;
-    *{$type} = sub () { $package };
-  }
-}
-
 use Mojo::Base -strict;
 
+use constant ();
 use Exporter 'import';
+use Mojo::Util;
+
+our @doc_types = (qw/
+  KeyInfo KeyDescriptor Organization ContactPerson
+  AssertionConsumerService SPSSODescriptor EntityDescriptor
+  AuthnRequest NameIDPolicy
+/);
+
+for my $type (@doc_types) {
+  my $package = "Mojo::SAML::Document::$type";
+  require(Mojo::Util::class_to_path($package));
+  constant->import($type => $package);
+}
+
 our %EXPORT_TAGS = (
   'docs' => [ @doc_types ],
 );
-our @EXPORT_OK = (@doc_types);
+our @EXPORT_OK = @doc_types;
 
 1;
 
